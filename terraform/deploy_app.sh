a02b9072f71ae1b9f49749e2f41bcc748f63e1c9
Vusi Pekane <vusumuzi@bbd.co.za>
2025-04-13 04:21:15 +0200

    changed workflow

---
+++ terraform/deploy_app.sh
@@ -0,0 +1,12 @@
+#install terraform
+
+sudo apt-add-repository --yes "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt update
+sudo apt install terraform
+
+echo "Running Terraform..."
+
+cd terraform
+echo "Running Terraform init..."
+terraform init -reconfigure
+echo "Done Running Terraform init..."
