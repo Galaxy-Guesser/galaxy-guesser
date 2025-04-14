7a9a62ea4abee610669663ca5aacbb9be2fbfc81
Nomcebo-Ndimande <sibongile@bbd.co.za>
2025-04-15 00:15:25 +0200

    populating db data

--- GalaxyGuesserApi/db/scripts/data/R__Categories.sql
+++ GalaxyGuesserApi/db/scripts/data/R__Categories.sql
@@ -6,5 +6,20 @@ VALUES
   ('Exoplanet Discoveries'),
   ('Galactic Structures'),
   ('Cosmic Mysteries'),
-  ('Space Exploration')
+  ('Space Exploration'),
+  ('Black Holes'),
+  ('The Solar System'),
+  ('Astrobiology'),
+  ('Cosmology'),
+  ('Space Telescopes'),
+  ('Missions to Mars'),
+  ('The Moon'),
+  ('Asteroids and Comets'),
+  ('Interstellar Medium'),
+  ('Gravitational Waves'),
+  ('Neutron Stars'),
+  ('The Big Bang'),
+  ('Alien Life Theories'),
+  ('Space Technology'),
+  ('Astronomical Instruments')
 ON CONFLICT (category) DO NOTHING;
