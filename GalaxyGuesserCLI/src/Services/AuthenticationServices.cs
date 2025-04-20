37923ba37e699b337e48bd9f8ef23749a8cc4dd3
Vusi Pekane <vusumuzi@bbd.co.za>
2025-04-20 15:39:47 +0200

    removed unused function

--- GalaxyGuesserCLI/src/Services/AuthenticationServices.cs
+++ GalaxyGuesserCLI/src/Services/AuthenticationServices.cs
@@ -36,30 +36,6 @@ namespace GalaxyGuesserCLI.Services
       return players;
     }
 
-    public static string ReadPassword()
-    {
-      StringBuilder password = new StringBuilder();
-      ConsoleKeyInfo key;
-
-      do
-      {
-        key = Console.ReadKey(true);
-
-        if (key.Key != ConsoleKey.Enter && key.Key != ConsoleKey.Backspace)
-        {
-          password.Append(key.KeyChar);
-          Console.Write("*");
-        }
-        else if (key.Key == ConsoleKey.Backspace && password.Length > 0)
-        {
-          password.Remove(password.Length - 1, 1);
-          Console.Write("\b \b");
-        }
-      } while (key.Key != ConsoleKey.Enter);
-
-      return password.ToString();
-    }
-
     internal async Task<Player> AuthOrRegisterWithBackend()
     {
       string jwt = Helper.GetStoredToken();
